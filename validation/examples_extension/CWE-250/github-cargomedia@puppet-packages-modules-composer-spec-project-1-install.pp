3ab88eb18d4fda19b7aa833bdda568190547ba88
Daniel Carvalho <daniel.m.carvalho2002@gmail.com>
2026-03-15 17:56:21 +0000

    fix: Changed semantic examples to only consider smells with 3 or more annotators agreement

--- validation/examples_extension/CWE-250/github-cargomedia@puppet-packages-modules-composer-spec-project-1-install.pp
+++
@@ -1,9 +0,0 @@
-node default {
-
-  composer::project{ 'seld/jsonlint':
-    target    => '/usr/local/lib/jsonlint',
-    user      => 'root',
-    version   => '1.2.0',
-    stability => 'dev',
-  }
-}
